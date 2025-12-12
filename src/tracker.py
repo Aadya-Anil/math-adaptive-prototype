8cc2e25238fa612926f2fe29b6441806582439bc
Aadya Anil Kumar <85759627+Aadya-Anil@users.noreply.github.com>
2025-12-12 16:56:04 +0530

    Update tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -1,5 +1,3 @@
-# src/tracker.py
-
 from dataclasses import dataclass, field
 
 @dataclass
@@ -17,12 +15,18 @@ class SessionTracker:
     def log(self, level, correct, time_taken, user_answer, true_answer):
         self.attempts.append(Attempt(level, correct, time_taken, user_answer, true_answer))
 
-    def accuracy(self):
+    def total_correct(self): #number of wrong answers in the current session
+        return sum(a.correct for a in self.attempts)
+
+    def total_wrong(self): #number of wrong answers in the current session
+        return len(self.attempts) - self.total_correct()
+        
+    def accuracy(self): #accuracy 
         if not self.attempts:
             return 0
         return sum(a.correct for a in self.attempts) / len(self.attempts)
 
-    def avg_time(self):
+    def avg_time(self): #average time
         if not self.attempts:
             return 0
         return sum(a.time_taken for a in self.attempts) / len(self.attempts)
