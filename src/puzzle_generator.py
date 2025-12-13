624ded8d92aba54f29b2994369949a41351477e1
Aadya Anil Kumar <85759627+Aadya-Anil@users.noreply.github.com>
2025-12-13 20:55:56 +0530

    Prevent fractions in puzzle expressions
    Added logic to avoid fractions in puzzle expressions for levels 0 and 2.

--- src/puzzle_generator.py
+++ src/puzzle_generator.py
@@ -8,6 +8,9 @@ def generate_puzzle(level):
         if op == "-" and b > a: # to avoid negative numbers because this is for ages 5-10
             a, b = b, a
         expr = f"{a} {op} {b}"
+         if op == "/": #avoids fraction answers
+            dividend = a * b
+            expr= f"{dividend} / a", b
 
     elif level == 1: # Medium : longer expressions
         a, b, c = random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)
@@ -18,8 +21,11 @@ def generate_puzzle(level):
         a, b, c = random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)
         op = random.choice(["+", "-","*","/"])
         expr = f"{a} {op} {b} {op} {c}"
+        if op == "/":
+            dividend = a * b
+            expr= f"{dividend} / a", b
 
-    else: # Warrior evel : combines two small expressions 
+    else: # Warrior level : combines two small expressions 
         a, b = random.randint(1, 9), random.randint(1, 9)
         c, d = random.randint(1, 9), random.randint(1, 9)
         expr = f"({a} + {b}) * ({c} - {d})"
